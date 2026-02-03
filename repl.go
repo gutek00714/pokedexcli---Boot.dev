f3d4911c6a61d52b72ae8bad6a07d5ff2c2b8acd
gutek00714 <guciolaczkowski1@gmail.com>
2026-02-03 02:07:54 +0100

    Add startRepl

--- repl.go
+++ repl.go
@@ -1,8 +1,29 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	return strings.Fields(lowered)
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		line := cleanInput(scanner.Text())
+		if len(line) == 0 {
+			continue
+		} else {
+			first_word := line[0]
+			fmt.Println("Your command was:", first_word)
+		}
+	}
 }
